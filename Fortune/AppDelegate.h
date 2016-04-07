f826e1fb970138636c12f63cc161ee60a7e87390
Michael Maloof 13' Pro <michaelmaloof1991@gmail.com>
2016-04-06 21:33:47 -0300

    Initial Commit

---
+++ Fortune/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fortune
+//
+//  Created by Michael Maloof on 4/6/16.
+//  Copyright © 2016 Jetpack Dinosaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fortune/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fortune
+//
+//  Created by Michael Maloof on 4/6/16.
+//  Copyright © 2016 Jetpack Dinosaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
